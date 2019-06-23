a639fb855a2cf7150cf173997ab882837653d3d9
stefan-schubert <stfn.j.schbrt@gmail.com>
2019-06-23 22:31:36 +0200

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '^.+\\.spec\\.ts$',
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts'],
+};
